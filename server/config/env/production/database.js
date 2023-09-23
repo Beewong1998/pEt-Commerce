80fed17a33211ff7035528462b9260a66b5f8194
Kin Wong <beewong2014@hotmail.com>
2023-09-23 13:40:14 +0100

    deploying strapi

--- server/config/env/production/database.js
+++ server/config/env/production/database.js
@@ -1,17 +1,16 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          ca: env('DATABASE_CA'),
-        },
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
